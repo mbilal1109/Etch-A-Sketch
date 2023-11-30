0042b888b01a805e7917f0881b5d4d26c6d09b5c
Mohammad Bilal <42048110+mbilal1109@users.noreply.github.com>
2023-11-29 20:56:22 -0600

    Initial file for etch-a-sketch

--- sketch.js
+++ sketch.js
@@ -2,6 +2,8 @@ const container = document.querySelector('#container');
 
 for(let i = 0; i < 16; i++) {
     const innerContainer = document.createElement('div');
+    
+    // To set a class to each div being created
     innerContainer.setAttribute("class", "inner-container");
     container.appendChild(innerContainer);
 }
